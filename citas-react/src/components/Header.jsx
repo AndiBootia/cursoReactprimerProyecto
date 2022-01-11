ce1eb61c24dc5b339acf6e4b0659166cabcce436
AndiBootia <acorrotti@bootia.dev>
2022-01-11 14:20:21 -0300

    cambios en el header

--- citas-react/src/components/Header.jsx
+++ citas-react/src/components/Header.jsx
@@ -1,8 +1,8 @@
 function Header(){
 
     return(
-        <h1 className="font-black text-4xl text-center">
-            Seguimiento pacientes 
+        <h1 className="font-black text-4xl text-center md:w-2/3 mx-auto">
+            Seguimiento pacientes {" "}
             <span className="text-indigo-600">Veterinaria</span>
         </h1>
 
